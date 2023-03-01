30045c6c534b32783cbade1ddc52bf1d7a6d8ff3
Coder23848 <103940315+Coder23848@users.noreply.github.com>
2023-03-01 09:39:59 -0500

    Not Really A Gamer

--- src/plugin/Plugin.cs
+++ src/plugin/Plugin.cs
@@ -1,14 +1,31 @@
 ﻿using BepInEx;
+using MoreSlugcats;
 
-namespace ExamplePlugin
+namespace NotReallyAGamer
 {
-    [BepInPlugin("com.example.examplemod", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInPlugin("com.coder23848.notreallyagamer", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        private void Awake()
+#pragma warning disable IDE0051 // Visual Studio is whiny
+        private void OnEnable()
         {
-            // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            On.Room.PlaySound_SoundID_float_float_float += Room_PlaySound_SoundID_float_float_float;
+        }
+#pragma warning restore IDE0051
+
+        private void Room_PlaySound_SoundID_float_float_float(On.Room.orig_PlaySound_SoundID_float_float_float orig, Room self, SoundID soundId, float pan, float vol, float pitch)
+        {
+            if (soundId == MoreSlugcatsEnums.MSCSoundID.Inv_GO)
+            {
+                orig(self, SoundID.UI_Slugcat_Die, pan, vol, pitch);
+            }
+            else if (soundId == MoreSlugcatsEnums.MSCSoundID.Inv_Hit)
+            {
+            }
+            else
+            {
+                orig(self, soundId, pan, vol, pitch);
+            }
         }
     }
 }
